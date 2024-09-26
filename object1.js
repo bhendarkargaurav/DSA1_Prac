92b5ccfeb7d8cdeccb696ae415335ddde2b58d1f
Gaurav <gaurav.bhe.24@gmail.com>
2024-09-26 08:52:46 +0530

    object1 is added

---
+++ object1.js
@@ -0,0 +1,22 @@
+// practice with apna college
+
+// create a cont object called " produc" to store info in thge pict
+
+// const Product = {
+//     productName : "Ball Pen",
+//     ProductValue: 280,
+//     Colour: "Black&white"
+// }
+// console.log(typeof Product);
+// console.log(Product);
+// console.log(Product.ProductValue);
+
+const bmwcar = {
+    price : 3000000,
+    colour : "black",
+    seater : "Two",
+    comfourt : "good"
+}
+
+console.log("thre is the feature of BMW", bmwcar);
+console.log("about the colour of bmw is", bmwcar.colour);
